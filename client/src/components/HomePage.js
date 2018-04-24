60e9b56c995ec8e112dda53bd18cbd4336dafee6
danielpinoga <daniel.pino@generalassemb.ly>
2018-04-24 14:26:49 -0400

    adding Link to homepage and loginpage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 
 class HomePage extends Component {
   render() {
     return (
       <div>
+
+        <div>
+          <Link to='/login'>Login Link</Link>
+        </div>
+
         HomePage
+
       </div>
     )
   }
--- client/src/components/LogInPage.js
+++ client/src/components/LogInPage.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class LogInPage extends Component {
   render() {
     return (
       <div>
+        <div>
+          <Link to='/'>Return Home</Link>
+        </div>
         LogInPage
       </div>
     )
