39642a43daf484c82af40fcbc25a7222be379f63
thedon122 <donovan122@yahoo.com>
2018-04-24 11:44:57 -0400

    added heroku

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ connection.on('error', (err) => {
 });
 
 app.use(bodyParser.json());
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+app.use(express.static(__dirname + '/client/build/'));
+  app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
