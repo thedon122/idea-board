b1f02bead90df3ac4bf1b1cfa628a2e060ac8e79
thedon122 <donovan122@yahoo.com>
2018-04-24 15:04:22 -0400

    done

--- client/src/components/LogInPage.js
+++ client/src/components/LogInPage.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
-class LogInPage extends Component {
+class LogIn extends Component {
+  state = {
+    users: []
+  }
+  componentDidMount() {
+    this.getAllUsers
+  }
+
+  getAllUsers = () => {
+    axios.get('api/users')
+    .then(res => {
+      console.log("saving users to state", res.data)
+      this.setState({ users: res.data })
+    })
+    .catch(err => {
+      console.console.error(err);
+      
+    })
+  }
+
   render() {
+    const userLinks = this.state.users.map((user, i )=> {
+      return
+      <div key={i}>
+      (<Link to={`/user/${user._id}`}>{user.userName}</Link>)
+      </div>
+    })
+  
     return (
       <div>
         <div>
           <Link to='/'>Return Home</Link>
         </div>
-        LogInPage
-      </div>
+        <h1>Log-In</h1>
+        <h3>Please Select an Existing User</h3>
+        {userLinks}
+        </div>
     )
   }
 }
 
-export default LogInPage
+export default LogIn
