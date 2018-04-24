033356e5fee6a2e2661d26817feeabee7ed9f169
danielpinoga <daniel.pino@generalassemb.ly>
2018-04-24 14:12:21 -0400

    added 3 components

---
+++ client/src/components/IdeaPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class IdeaPage extends Component {
+  render() {
+    return (
+      <div>
+        IdeaPage
+      </div>
+    )
+  }
+}
+
+export default IdeaPage
---
+++ client/src/components/LogInPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LogInPage extends Component {
+  render() {
+    return (
+      <div>
+        LogInPage
+      </div>
+    )
+  }
+}
+
+export default LogInPage
